de7e36a460e472eef3780a0eaa6194e80710b50a
 Seliverstov <seliverstov@mail.ee>
2022-08-02 14:06:28 +0300

    make regexp filter

--- handlers.py
+++ handlers.py
@@ -1,8 +1,9 @@
-from aiogram import types
-from aiogram.dispatcher import filters
+import re
 
-from main import dp, bot
-from save_and_load import load_json, save_json
+from aiogram import types
+
+from main import dp
+from save_and_load import load_json, save_json, make_sum
 
 
 @dp.message_handler(commands=['start'])
@@ -17,32 +18,16 @@ async def process_start_command(message: types.Message):
 
 
 @dp.message_handler(commands=['reset'])
-async def reset(message: types.Message):
+async def reset(msg: types.Message):
     data = load_json()
-    user_id = str(message.from_user.id)
-    data[user_id] = 0
+    data[str(msg.chat.id)] = 0
     save_json(data)
-    await message.reply(str(data[user_id]))
+    await msg.reply('Сброс')
 
 
-@dp.message_handler(filters.Text(endswith='₽'))
-async def counter(msg: types.Message):
-    try:
-        number = float(msg.text.rstrip('₽'))
-        data = load_json()
-        user_id = str(msg.from_user.id)
-        try:
-            data[user_id] = round(number + data[user_id], 2)
-        except KeyError:
-            data[user_id] = number
-        save_json(data)
-        await bot.send_message(msg.from_user.id,
-                               f"Сумма: *{str(data[user_id]).rstrip('0').rstrip('.')}*",
-                               parse_mode="MarkdownV2")
-    except ValueError:
-        await not_recognised()
+@dp.message_handler(regexp=re.compile(r'(^|\s)[-+]?\d+[р₽]\b', re.IGNORECASE))
+async def counter(msg: types.Message, regexp: re.match):
+    number = int(regexp[0][:-1])
+    chat_sum = make_sum(msg.chat.id, number)
+    await msg.answer(f"Сумма: *{chat_sum}*", parse_mode="MarkdownV2")
 
-
-@dp.message_handler()
-async def not_recognised(msg: types.Message):
-    await bot.send_message(msg.from_user.id, 'Принимаю только рубли.')
--- save_and_load.py
+++ save_and_load.py
@@ -17,3 +17,14 @@ def save_json(data):
         os.mkdir('data')
     with open('data/data.json', 'w', encoding='utf-8') as f:
         f.write(json.dumps(data, ensure_ascii=False))
+
+
+def make_sum(chat_id: int, number: int) -> int:
+    chat_id = str(chat_id)
+    data = load_json()
+    try:
+        data[chat_id] += number
+    except KeyError:
+        data[chat_id] = number
+    save_json(data)
+    return data[chat_id]
